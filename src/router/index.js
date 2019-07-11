db4c016b71c15573e82435783a76e15419cb32e7
shishishi8888 <1186324052@qq.com>
2019-07-11 11:50:43 +0800

    加了个头部

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Mine from '../sjy/mine/mineindex.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/mine',
+      name: 'Mine',
+      component: Mine
+    }
   ]
 })
